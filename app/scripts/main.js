3a92020695c360c54768610637f1887c6832e0ca
Dillon Fosa <dillonfosa@gmail.com>
2017-02-11 19:52:22 -0500

    made changes in navbar and added background zoom image

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  $(".parallax-zoom-blur img").css({
+    width: (80 + scroll/5)  + "%",
+    top: -(scroll/10)  + "%",
+    "-webkit-filter": "blur(" + (scroll/80) + "px)",
+    filter: "blur(" + (scroll/80) + "px)"
+  });
+});
